f4de218171c3d67079b5520ac5f418b8333cef15
Jitin J Gigi <37440750+madhaven@users.noreply.github.com>
2019-10-17 13:08:01 +0530

    Add files via upload

--- source/validate.js
+++ source/validate.js
@@ -1,4 +1,7 @@
 function checkEmail(text){
-  // console.log("WORKING");
-  return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(text))
+    // console.log("WORKING");
+    return (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(text))
+}
+function checkPassword(text){
+    return (/^.{8,}$/.test(text));
 }
